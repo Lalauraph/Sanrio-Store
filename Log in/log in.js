efcff265907f79b8e2c2f28c9145986baf6c3cae
majito <mariajorm2004@gmail.com>
2023-11-01 19:12:21 -0500

    login

--- Log in/log in.js
+++ Log in/log in.js
@@ -1,19 +1,19 @@
-const render = () => {
-    const formulario = document.querySelector("#iniciar-sesion");
+// const render = () => {
+//     const formulario = document.querySelector("#iniciar-sesion");
     
-    formulario.addEventListener("submit", (event) => {
-        event.preventDefault();
-        const email = event.target.email.value;
-        const password = event.target.password.value;
-        console.log (email,password);
-    });
+//     formulario.addEventListener("submit", (event) => {
+//         event.preventDefault();
+//         const email = event.target.email.value;
+//         const password = event.target.password.value;
+//         console.log (email,password);
+//     });
     
-    const inputEmail = document.querySelector("#email");
-    inputEmail.addEventListener("input", (e) => {
-        console.log(e.target.value);  //capturar un evento en vivo
-    })
+//     const inputEmail = document.querySelector("#email");
+//     inputEmail.addEventListener("input", (e) => {
+//         console.log(e.target.value);  //capturar un evento en vivo
+//     })
     
-    }
+//     }
     
     //cargar el javascript cuando ya se tiene todo en html y en css
     window.onload = render;
